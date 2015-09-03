6010b472eeac47722a8a11fa92a325121d337034
brianotoole <botoole518@gmail.com>
2015-09-03 15:35:04 -0400

    sass setup

--- readme.md
+++ readme.md
@@ -13,10 +13,10 @@ Full site redesign
 2. Uses Sass. Main stylesheet is `scss/style.scss` *To compile, run:*
   
   
-  `sass--watch scss/style.scss:css/style.css`
+  `sass --watch assets/scss/style.scss:style.css`
   
   *To compile as compressed, run:*
 
   
-  `sass--watch scss/style.scss:css/style.css --style compressed`
+  `sass--watch assets/scss/style.scss:style.css --style compressed`
  
--- wp-content/themes/brianzotoole2-0/functions.php
+++ wp-content/themes/brianzotoole2-0/functions.php
@@ -8,38 +8,16 @@
  */
 
 if ( ! function_exists( 'brianzotoole2_0_setup' ) ) :
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- */
+
 function brianzotoole2_0_setup() {
-	/*
-	 * Make theme available for translation.
-	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on brianzotoole2.0, use a find and replace
-	 * to change 'brianzotoole2-0' to the name of your theme in all the template files.
-	 */
+
 	load_theme_textdomain( 'brianzotoole2-0', get_template_directory() . '/languages' );
 
 	// Add default posts and comments RSS feed links to head.
 	add_theme_support( 'automatic-feed-links' );
 
-	/*
-	 * Let WordPress manage the document title.
-	 * By adding theme support, we declare that this theme does not use a
-	 * hard-coded <title> tag in the document head, and expect WordPress to
-	 * provide it for us.
-	 */
 	add_theme_support( 'title-tag' );
 
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
-	 *
-	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-	 */
 	add_theme_support( 'post-thumbnails' );
 
 	// This theme uses wp_nav_menu() in one location.
@@ -47,10 +25,6 @@ function brianzotoole2_0_setup() {
 		'primary' => esc_html__( 'Primary Menu', 'brianzotoole2-0' ),
 	) );
 
-	/*
-	 * Switch default core markup for search form, comment form, and comments
-	 * to output valid HTML5.
-	 */
 	add_theme_support( 'html5', array(
 		'search-form',
 		'comment-form',
@@ -59,10 +33,6 @@ function brianzotoole2_0_setup() {
 		'caption',
 	) );
 
-	/*
-	 * Enable support for Post Formats.
-	 * See https://developer.wordpress.org/themes/functionality/post-formats/
-	 */
 	add_theme_support( 'post-formats', array(
 		'aside',
 		'image',
@@ -80,23 +50,12 @@ function brianzotoole2_0_setup() {
 endif; // brianzotoole2_0_setup
 add_action( 'after_setup_theme', 'brianzotoole2_0_setup' );
 
-/**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
 function brianzotoole2_0_content_width() {
 	$GLOBALS['content_width'] = apply_filters( 'brianzotoole2_0_content_width', 640 );
 }
 add_action( 'after_setup_theme', 'brianzotoole2_0_content_width', 0 );
 
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
+
 function brianzotoole2_0_widgets_init() {
 	register_sidebar( array(
 		'name'          => esc_html__( 'Sidebar', 'brianzotoole2-0' ),
@@ -114,7 +73,7 @@ add_action( 'widgets_init', 'brianzotoole2_0_widgets_init' );
  * Enqueue scripts and styles.
  */
 function brianzotoole2_0_scripts() {
-	wp_enqueue_style( 'brianzotoole2-0-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'main', get_stylesheet_uri(), array(), '2.0');
 
 	wp_enqueue_script( 'brianzotoole2-0-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
@@ -126,27 +85,13 @@ function brianzotoole2_0_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'brianzotoole2_0_scripts' );
 
-/**
- * Implement the Custom Header feature.
- */
+
 require get_template_directory() . '/inc/custom-header.php';
 
-/**
- * Custom template tags for this theme.
- */
 require get_template_directory() . '/inc/template-tags.php';
 
-/**
- * Custom functions that act independently of the theme templates.
- */
 require get_template_directory() . '/inc/extras.php';
 
-/**
- * Customizer additions.
- */
 require get_template_directory() . '/inc/customizer.php';
 
-/**
- * Load Jetpack compatibility file.
- */
 require get_template_directory() . '/inc/jetpack.php';
