a0564b3ce2ed2485781b38405daf70c210c1b421
brianotoole <botoole518@gmail.com>
2015-09-03 17:25:42 -0400

    sections setup & styling

--- wp-content/themes/brianzotoole2-0/template-parts/content-home.php
+++ wp-content/themes/brianzotoole2-0/template-parts/content-home.php
@@ -2,9 +2,9 @@
   <div class="full clearfix">
     <div class="grid_12">
       <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/brianzotoole.jpg" class="animated bounceIn circle" alt="brian o'toole" />
-	  <h2>Brian Z<span>.</span> O'Toole</h2>
+	  <h1 class="title">Brian Z. O'Toole</h1>
 	  <hr class="star">
-	  <p><span>{I am a}</span> digital projects director &amp; web developer. With skills ranging from web &amp; mobile development to creative strategy, I strive to be as multidimensional as possible. I love minimalistic, responsive design that provides a low-bandwidth user-experience.</p>
+	  <p class="skills">Web Developer - Project Manager - Creative Strategist</p>
     </div><!--/.grid-->
   </div><!--/.container-->
 </div><!--/.slide .about #slide1-->
@@ -13,7 +13,9 @@
 <div class="slide articles" id="slide2" data-slide="2" data-stellar-background-ratio="0.5">
   <div class="full clearfix">
     <div class="grid_12">
-	  <p>Learn With Me</p>
+	  <h2 class="title">Learn With Me</h2>
+	  <hr class="star">
+	  <p>I am a digital projects director &amp; web developer located in Tampa, Florida. My skills range from web development to creative strategy. I enjoy using responsive techniques to build websites that provide a low-bandwidth and meaningful user experience.</p>
     </div><!--/.grid-->
   </div><!--/.container-->
 </div><!--/.slide .articles #slide4-->
@@ -22,7 +24,8 @@
 <div class="slide work" id="slide3" data-slide="3" data-stellar-background-ratio="0.5">
   <div class="full clearfix">
     <div class="grid_12">
-	  <p>Recent Work</p>
+	  <h2 class="title">Recent Work</h2>
+	  <hr class="star">
     </div><!--/.grid-->
   </div><!--/.container-->
 </div><!--/.slide .work #slide3-->
@@ -31,7 +34,8 @@
 <div class="slide contact" id="slide4" data-slide="4" data-stellar-background-ratio="0.5">
   <div class="full clearfix">
     <div class="grid_12">
-	  <p>Let's Talk</p>
+	  <h2 class="title">Let's Talk</h2>
+	  <hr class="star">
     </div><!--/.grid-->
   </div><!--/.container-->
 </div><!--/.slide .contact #slide4-->
