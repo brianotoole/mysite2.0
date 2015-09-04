1c7b3e1a1b15cde0221e235f7429f2a30e70bf17
botoole <botoole518@gmail.com>
2015-09-04 17:59:48 -0400

    removed thumb img from articles loop (cleaner)

--- wp-content/themes/brianzotoole2-0/template-parts/content-latest_articles.php
+++ wp-content/themes/brianzotoole2-0/template-parts/content-latest_articles.php
@@ -12,24 +12,9 @@ $loop = new WP_Query( $args );
 if ( $loop->have_posts() ) {
 	while ( $loop->have_posts() ) {
 		$loop->the_post(); ?>
-		<div class="grid_11">
+		<div class="grid_12">
 		  <a href="<?php the_permalink() ?>"><h4 class="post-title"><?php the_title(); ?></h4></a>
 		  <?php the_excerpt('my_excerpt_length'); ?>
-		</div><!--/.grid-->
-		<div class="grid_1 omega">
-		  <?php if (has_post_thumbnail( $post->ID ) ): //if featured image is uploaded... ?>
-				  <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); $image = $image[0]; ?>
-		    <a href="<?php the_permalink() ?>">
-		      <img class="thumbnail circle" src="<?php echo $image; ?>">
-		    </a>
-		  
-		  <?php else: //if no featured image is uploaded, show default icon img ?>
-		    <a href="<?php the_permalink() ?>">
-		      <div class="thumbnail default circle">
-		        <i class="icon-magic"></i>
-		      </div>
-		    </a>
-		  <?php endif; ?>
 		</div><!--/.grid-->
 		<div class="clearfix"></div><hr />
 
