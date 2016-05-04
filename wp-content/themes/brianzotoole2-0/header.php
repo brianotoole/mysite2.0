1d7f814618f902ee64f081b9006603a4ee451e0d
brianotoole <botoole518@gmail.com>
2016-05-04 17:29:59 -0400

    move ga to footer

--- wp-content/themes/brianzotoole2-0/header.php
+++ wp-content/themes/brianzotoole2-0/header.php
@@ -13,16 +13,6 @@
 <meta name="description" content="A Projects Director & Web Developer living in Tampa, FL">
 <meta name="keywords" content="Tampa Website Design, Web Design Tampa, Web Development Tampa, Tampa website help, Web Designers in Tampa, Responsive Design Tampa">
 <?php wp_head(); ?>
-<script>
-//GA
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-45683618-1', 'auto');
-  ga('send', 'pageview');
-</script>
 </head>
 
 <body <?php body_class(); ?>>
